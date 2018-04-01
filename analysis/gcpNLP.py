235dbdf977f158a514d34861fd3764869b34230b
Anirudh Balasubramaniam <anirudhnkl@gmail.com>
2018-04-01 05:52:37 -0700

    answer choices

--- analysis/gcpNLP.py
+++ analysis/gcpNLP.py
@@ -5,6 +5,7 @@ from google.cloud.language import types
 from operator import itemgetter
 from aylienapiclient import textapi
 import json
+import scrape
 
 
 # # Instantiates a client
@@ -33,6 +34,11 @@ def getQuestionData(text):
     #textString = 'At a time when European kingdoms were beginning to establish new trade routes and colonies, motivated by imperialism and economic competition, Columbus proposed to reach the East Indies (South and Southeast Asia) by sailing westward. This eventually received the support of the Spanish Crown, which saw a chance to enter the spice trade with Asia through this new route. During his first voyage in 1492, he reached the New World instead of arriving in Japan as he had intended, landing on an island in the Bahamas archipelago that he named San Salvador. Over the course of three more voyages, he visited the Greater and Lesser Antilles, as well as the Caribbean coast of Venezuela and Central America, claiming all of it for the Crown of Castile. Though preceded by short-lived Norse colonization of North America led by Leif Erikson in the 11th century,[5][6] Columbus is the European explorer credited with establishing and documenting routes to the Americas, securing lasting European ties to the Americas, and inaugurating a period of exploration, conquest, and colonization that lasted for centuries. His exertions thereby strongly contributed to the development of the modern Western world. He also founded the transatlantic slave trade and has been accused by several historians of initiating the genocide of the Hispaniola natives. Columbus himself saw his accomplishments primarily in the light of spreading the Catholic religion.[7] Columbus had set course in hopes of finding a western route to the Indies (Asia). He called the inhabitants of the lands that he visited indios Spanish for Indians. His strained relationship with the Spanish crown and its appointed colonial administrators in America led to his arrest and dismissal as governor of the settlements on the island of Hispaniola in 1500, and later to protracted litigation over the benefits that he and his heirs claimed were owed to them by the crown.'
 
     textString = text
+
+    document = types.Document(
+        content=textString,
+        type=enums.Document.Type.PLAIN_TEXT)
+    allEntities = client.analyze_entities(document).entities
 
     #textString = "DEVELOPMENT OF THE ECONOMIC THEORY OF VALUE Because economic activity has been a central feature of all societies, it is surprising that these activities were not studied in any detail until recently. For the most part, economic phenomena were treated as a basic aspect of human behavior that was not sufficiently interesting to deserve specific attention. It is, of course, true that individuals have always studied economic activities with a view toward making some kind of personal gain. Roman traders were not above making profits on their transactions. But investigations into the basic nature of these activities did not begin in any depth until the eighteenth century.3 Because this book is about economic theory as it stands today, rather than the history of economic thought, our discussion of the evolution of economic theory will be brief. Only one area of economic study will be examined in its historical setting: the theory of value. Early economic thoughts on value The theory of value, not surprisingly, concerns the determinants of the “value” of a commodity. This subject is at the center of modern microeconomic theory and is closely intertwined with the fundamental economic problem of allocating scarce resources to alternative uses. The logical place to start is with a definition of the word “value.” Unfortunately, the meaning of this term has not been consistent throughout the development of the subject. Today we regard value as being synonymous with the price of a commodity.4 Earlier philosopher-economists, however, made a distinction between the market price of a commodity and its value. The term “value” was then thought of as being, in some sense, synonymous with “importance,” “essentiality,” or (at times) “godliness.” Because “price” and “value” were separate concepts, they could differ, and most early economic discussions centered on these divergences. For example, St. Thomas Aquinas believed value to be divinely determined. Since prices were set by humans, it was possible for the price of a commodity to differ from its value. A person accused of charging a price in excess of a good’s value was guilty of charging an “unjust” price. For example, St. Thomas believed the “just” rate of interest to be zero. Any lender who demanded a payment for the use of money was charging an unjust price and could be—and sometimes was—prosecuted by church officials. The founding of modern economics During the latter part of the eighteenth century, philosophers began to take a more scientific approach to economic questions. The 1776 publication of The Wealth of Nations by Adam Smith (1723–1790) is generally considered the beginning of modern economics. In his vast, all-encompassing work, Smith laid the foundation for thinking about market forces in an ordered and systematic way. Still, Smith and his immediate successors, such as David Ricardo (1772–1823), continued to distinguish between value and price. To Smith, for example, the value of a commodity meant its “value in use,” whereas the price represented its “value in exchange.” The distinction between these two concepts was illustrated by the famous waterdiamond paradox. Water, which obviously has great value in use, has little value in exchange (it has a low price); diamonds are of little practical use but have a great value in exchange. The paradox with which early economists struggled derives from the observation that some very useful items have low prices whereas certain nonessential items have high prices."
     #summary = client.Summarize({"text": "At a time when European kingdoms were beginning to establish new trade routes and colonies, motivated by imperialism and economic competition, Columbus proposed to reach the East Indies (South and Southeast Asia) by sailing westward. This eventually received the support of the Spanish Crown, which saw a chance to enter the spice trade with Asia through this new route. During his first voyage in 1492, he reached the New World instead of arriving in Japan as he had intended, landing on an island in the Bahamas archipelago that he named San Salvador. Over the course of three more voyages, he visited the Greater and Lesser Antilles, as well as the Caribbean coast of Venezuela and Central America, claiming all of it for the Crown of Castile. Though preceded by short-lived Norse colonization of North America led by Leif Erikson in the 11th century,[5][6] Columbus is the European explorer credited with establishing and documenting routes to the Americas, securing lasting European ties to the Americas, and inaugurating a period of exploration, conquest, and colonization that lasted for centuries. His exertions thereby strongly contributed to the development of the modern Western world. He also founded the transatlantic slave trade and has been accused by several historians of initiating the genocide of the Hispaniola natives. Columbus himself saw his accomplishments primarily in the light of spreading the Catholic religion.[7] Columbus had set course in hopes of finding a western route to the Indies (Asia). He called the inhabitants of the lands that he visited indios Spanish for Indians. His strained relationship with the Spanish crown and its appointed colonial administrators in America led to his arrest and dismissal as governor of the settlements on the island of Hispaniola in 1500, and later to protracted litigation over the benefits that he and his heirs claimed were owed to them by the crown."})
@@ -75,6 +81,8 @@ def getQuestionData(text):
     scores = []
     questions = []
     answers = []
+    choices = {}
+    fullObject = {}
     optimalWord = ""
 
     # for entity in entries:
@@ -111,14 +119,79 @@ def getQuestionData(text):
                 score += 1
             scores[i] = score
         
+        maxEntity = sentenceEntities[scores.index(max(scores))]
         maxWord = sentenceEntities[scores.index(max(scores))].name
         answers.append(maxWord)
+        # index = sentence.find(maxWord)
+        # print("\n" + maxWord + "\n" + sentence[:index] + " ____ " + sentence[index + len(maxWord) + 1:])
+        options = []
+        # for allE in allEntities:
+        #     if entity_type[allE.type] == entity_type[maxEntity.type] and noun_type[allE.mentions[0].type] == noun_type[maxEntity.mentions[0].type]:
+        #         options.append(allE)
+        
+        options.sort(key=lambda x: x.salience, reverse=True)
+        count = 0
+        inChoices = False
+        choices = {}
+        choices[maxWord] = []
+
+        choices[maxWord] = scrape.getAnswerChoices(maxWord)
+
+        if not choices[maxWord]:
+            for o in allEntities:
+                if count >= 3:
+                    break
+                if entity_type[o.type] == entity_type[maxEntity.type] and noun_type[o.mentions[0].type] == noun_type[maxEntity.mentions[0].type]:
+                    for c in choices[maxWord]:
+                        if(o.name.lower() == c.lower()):
+                            inChoices = True
+                            break
+                    if o.name.lower() != maxWord.lower() and not inChoices:
+                        choices[maxWord].append(o.name)
+                        count += 1
+
+        count = len(choices[maxWord])                
+        if count < 3:
+            for o in allEntities:
+                if count >= 3:
+                    break
+                if entity_type[o.type] == entity_type[maxEntity.type]:
+                    for c in choices[maxWord]:
+                        if(o.name.lower() == c.lower()):
+                            inChoices = True
+                            break
+                if o.name.lower() != maxWord.lower() and not inChoices:
+                    choices[maxWord].append(o.name)
+                    count += 1
+
+        count = len(choices[maxWord])                
+        if count < 3:
+            for o in allEntities:
+                if count >= 3:
+                    break
+                if noun_type[o.mentions[0].type] == noun_type[maxEntity.mentions[0].type]:
+                    for c in choices[maxWord]:
+                        if(o.name.lower() == c.lower()):
+                            inChoices = True
+                            break
+                if o.name.lower() != maxWord.lower() and not inChoices:
+                    choices[maxWord].append(o.name)
+                    count += 1           
+                    
         index = sentence.find(maxWord)
-        questions.append(sentence[:index] + " ____ " + sentence[index + len(maxWord) + 1:])
+        question = sentence[:index] + " ____ " + sentence[index + len(maxWord) + 1:]
+        questions.append(question)
+
+        fullObject[question] = choices
 
     sentenceEntities = []
     scores = []
     score = 0
+
+    # for k, v in fullObject.items():
+    #     print(k + " ")
+    #     print(v)
+    #     print("\n")
 
     for word in entries:
         if noun_type[word.mentions[0].type] == 'PROPER':
@@ -129,6 +202,8 @@ def getQuestionData(text):
 
             sentenceEntities.append(word)
             scores.append(score)
+    
+    numMCQ = len(questions)
 
     if len(scores) > 0 and len(sentenceEntities) > 0:
         sortedEntities = [list(x) for x in zip(*sorted(zip(scores, sentenceEntities), key=itemgetter(0)))][1]
@@ -160,6 +235,9 @@ def getQuestionData(text):
 
     data = []
     for i in range(len(questions)):
-        data.append({"Question": questions[i], "Correct": answers[i], "Answers": [" ", " ", " "]}) #answer_choices[i]})
+        if i < numMCQ:
+            data.append({"Question": questions[i], "Correct": answers[i], "Answers": fullObject[questions[i]][answers[i]]}) #answer_choices[i]})
+        else:
+            data.append({"Question": questions[i], "Correct": answers[i], "Answers": ["","",""]})
 
     return data
